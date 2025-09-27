1ddddc95a6632f47ca2f9c547c79f9465266bf2f
ustrungnguyen <contact@ngtrung.com>
2025-09-28 04:45:46 +0700

    Adding pages & components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,28 @@
+// Importing Libraries
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { useState, useEffect } from 'react';
+
+// Importing Pages
+import Home from './pages/Home/Home';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Form from './pages/Form/Form';
+
+// Importing CSS
 import './App.css';
 
 export default function App() {
-  const [number, setNumber] = useState(null);
-  useEffect(() => {
-
-  }, [])
 
   return (
     <>
-
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/form' element={<Form />} />
+          </Routes>
+        </Router>
+      </div>
     </>
   )
 }
---
+++ frontend/src/pages/Form/Form.jsx
@@ -0,0 +1,9 @@
+import './form.css';
+
+export default function Form() {
+    return (
+        <div className='form'>
+            
+        </div>
+    )
+}
