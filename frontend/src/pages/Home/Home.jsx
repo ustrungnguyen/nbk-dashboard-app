da699d424782183f86e5e18299911362235ecedc
ustrungnguyen <contact@ngtrung.com>
2025-09-28 07:34:37 +0700

    Updated animation image

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,3 +1,7 @@
+// Importing Images
+import WelcomeImage from '../../assets/Home/react.png';
+
+// Importing CSS
 import './home.css';
 
 export default function Home() {
@@ -9,10 +13,13 @@ export default function Home() {
                 <div className='grid-background'></div>
 
                 {/* Welcome section content */}
-                <div className='content'>
+                <div className='welcome-content-text'>
                     <h1>Unlock the Power of Your Data</h1>
                     <p>Join thousands of businesses using AI-driven insights.</p>
                 </div>
+
+                {/* Animation image */}
+                <img src={WelcomeImage} alt="Welcome" className='welcome-image' />
             </section>
         </main>
     )
