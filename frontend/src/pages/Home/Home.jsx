7b1b9986e6b323bf0e9e601935329a06f705ea3e
ustrungnguyen <contact@ngtrung.com>
2025-09-28 07:02:52 +0700

    Updated Home Page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,13 +2,18 @@ import './home.css';
 
 export default function Home() {
     return (
-        <div className='home'>
-            <div className='grid-background'></div>
+        <main className='home-container'>
+            {/* Welcome Section */}
+            <section className='welcome-section'>
+                {/* The grid background */}
+                <div className='grid-background'></div>
 
-            <div className='content'>
-                <h1>Unlock the Power of Your Data</h1>
-                <p>Join thousands of businesses using AI-driven insights.</p>
-            </div>
-        </div>
+                {/* Welcome section content */}
+                <div className='content'>
+                    <h1>Unlock the Power of Your Data</h1>
+                    <p>Join thousands of businesses using AI-driven insights.</p>
+                </div>
+            </section>
+        </main>
     )
 }
