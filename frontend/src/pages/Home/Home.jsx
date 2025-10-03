99c48ecf8ca4f01e8334bc204f9d8e7ec7f574f0
ustrungnguyen <contact@ngtrung.com>
2025-10-04 00:23:03 +0700

    Updated responsive layout & home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,15 +1,23 @@
 // Importing React
 import React from 'react';
 
+// Importing custom hooks
+import useInView from '../../custom_hooks/useInView';
+
+// Importing modules
+import cn from 'classnames';
+
 // Importing Images
 import WelcomeImage from '../../assets/Home/react.png';
+import DashboardImage from '../../assets/Home/DashboardConner.png';
 
 // Importing CSS
 import './home.css';
 import './home_animation.css';
 
 export default function Home() {
-
+    const [headingRef, headingInView] = useInView({ threshold: 0.5 });
+    const [contentRef, contentInView] = useInView({ threshold: 0.5 });
 
     return (
         <>
@@ -40,24 +48,91 @@ export default function Home() {
 
                 {/* Dashboard Overview Section */}
                 <section className='dashboard-overview-section'>
+                        <div className='dashboard-overview-container'>
 
-                    {/* Dashboard overview heading container */}
-                    <div className='dashboard-overview-heading'>
+                        {/* Dashboard overview heading container */}
+                        <div
+                            ref={headingRef}
+                            className='dashboard-overview-heading'>
 
-                        {/* Dashboard overview heading badge */}
-                        <div className='dashboard-overview-badge'>
-                            Tổng quan
+                            {/* Dashboard overview heading badge */}
+                            <div 
+                                    className={`
+                                    dashboard-overview-badge 
+                                    dashboard-overview-badge-animation 
+                                    ${headingInView ? 'is-in-view' : ''}`}>
+                                Tổng quan
+                            </div>
+
+                            {/* Dashboard overview title */}
+                            <h1 
+                                className={`
+                                    dashboard-overview-title 
+                                    dashboard-overview-title-animation 
+                                    ${headingInView ? 'is-in-view' : ''}`}>
+                                Tổng quan sản phẩm
+                            </h1>
                         </div>
 
-                        {/* Dashboard overview title */}
-                        <h1 className='dashboard-overview-title'>Tổng quan sản phẩm</h1>
+                        {/* Dashboard overview content */}
+                        <div
+                            ref={contentRef}
+                            className={cn(
+                                'dashboard-overview-content',
+                                'dashboard-overview-content-animation',
+                                {'is-in-view': contentInView}
+                        )}>
+                            <p>
+                                Dashboard của chúng tôi cung cấp cái nhìn tổng quan về quá trình học tập của bạn, giúp bạn theo dõi tiến trình và hiệu suất học tập một cách dễ dàng. Với giao diện trực quan và các biểu đồ, bạn có thể nhanh chóng nắm bắt được những điểm mạnh và điểm cần cải thiện trong quá trình học tập.
+                            </p>
+                        </div>
                     </div>
+                </section>
 
-                    {/* Dashboard overview content */}
-                    <div className='dashboard-overview-content'>
-                        <p>
-                            Dashboard của chúng tôi cung cấp cái nhìn tổng quan về quá trình học tập của bạn, giúp bạn theo dõi tiến trình và hiệu suất học tập một cách dễ dàng. Với giao diện trực quan và các biểu đồ, bạn có thể nhanh chóng nắm bắt được những điểm mạnh và điểm cần cải thiện trong quá trình học tập.
-                        </p>
+                {/* Dashboard Features Section */}
+                <section className='dashboard-features-section'>
+                    <div className='dashboard-features-container'>
+                        {/* Dashboard features heading container */}
+                        <div className='dashboard-features-heading-grid'>
+                            <div className='dashboard-features-heading-container'>
+                                <h1 className='dashboard-features-title-animation'>Giao diện của Dashboard</h1>
+                            </div>
+
+                            <div className='dashboard-features-content-container'>
+                                <p className='dashboard-features-content-animation'>
+                                    Giao diện của dashboard được thiết kế để cung cấp trải nghiệm người dùng tối ưu. Bạn có thể tùy chỉnh và cá nhân hóa dashboard theo nhu cầu riêng của mình.
+                                </p>
+                            </div>
+                        </div>
+
+                        {/* Dashboard features image container */}
+                        <div className='dashboard-features-image-container'>
+                            <img className='dashboard-features-image-animation' src={DashboardImage} alt="Dashboard" />
+                        </div>
+                    </div>
+                </section>
+
+                {/* Updates Summary */}
+                <section className='updates-summary-section'>
+                    <div className='updates-summary-container'>
+
+                        {/* Updates heading container */}
+                        <div className='updates-heading-container'>
+                            <div
+                                className='updates-badge'>
+                            Mới nhất</div>
+
+                            <h1>
+                                Các cập nhật mới nhất
+                            </h1>
+                        </div>
+
+                        {/* Updates caption container */}
+                        <div className='updates-caption-container'>
+                            <div>
+                                Dashboard liên tục cập nhật các tính năng theo dõi tiến trình học tập, phân tích hiệu suất cá nhân, hiển thị biểu đồ trực quan và cho phép tùy chỉnh giao diện theo nhu cầu của người dùng.
+                            </div>
+                        </div>
                     </div>
                 </section>
             </main>
