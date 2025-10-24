183c4e746ca480a992d1bc2eb895403babcf98c3
ustrungnguyen <contact@ngtrung.com>
2025-10-24 23:46:02 +0700

    Raised max token

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ export default function App() {
           {/* Header */}
           <Header />
 
-          {/* Routers */}
+          {/* Routes */}
           <main>
               <Routes>
                 <Route path='/' element={<Home />} />
